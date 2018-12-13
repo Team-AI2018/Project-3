106e2c218f71a4eaaecd943389448b6247f19c14
Angel Wu <willjxc@gmail.com>
2018-12-13 09:36:52 -0500

    added authetication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Axios from 'axios';
+import UserService from '../service/UserService';
+import {Link} from 'react-router-dom'
 
-const SignUp = () => {
-    return(
+class Login extends Component {
+  state = { usernameInput: '', passwordInput: '' };
+  userService = new UserService();
+
+
+handleChange = (e) =>{
+    this.setState({[e.target.name]: e.target.value})
+}
+
+
+handleFormSubmit = (e) =>{
+    e.preventDefault();
+    // you could just do axios.post('http://localhost:5000/api/signup, {username: this.state.userNameInput, password: this.state.passWordInput}, {withCredentials: true})
+    this.userService.login(this.state.usernameInput, this.state.passwordInput)
+    .then((userFromDB)=>{
+        // here we wait for the API to give us the user object back after logging in
+        this.setState({usernameInput: '', passwordInput: ''})
         
-        <form className="ui form">
+        console.log(userFromDB)
+        // then we pass that user object back to app component
+        this.props.logTheUserIntoAppComponent(userFromDB)
+        // here, we are getting the user object from the DB
+        // and we're setting AppComponent.state.loggedinuser equal to it
+
+
+        //this.props.history.push('/project-index');
+        // then we redirect
+        
+
+
+    })
+    .catch((err)=>{
+        console.log('sorry something went wrong', err)
+
+    })
+
+}
+
+render(){
+  return(
+    <div>
+        <form className="ui form" onSubmit={this.handleFormSubmit}>
         <div className="field">
           <label>Username</label>
-          <input type="text" name="first-name" placeholder="First Name"/>
+          <input type="text" name="usernameInput" value={this.state.usernameInput} onChange={ e => this.handleChange(e)}/>
         </div>
         <div className="field"> 
           <label>Password</label>
-          <input type="password" name="password" placeholder="password"/>
+          <input type="password" name="passwordInput" value={this.state.passwordInput} onChange={ e => this.handleChange(e)}/>
         </div>
        
 
@@ -20,8 +61,36 @@ const SignUp = () => {
         </div>
 
       </form>
-      
-    )
-};
 
-export default SignUp;
+</div>
+  )
+}
+}
+
+
+
+// const Login = () => {
+//     return(
+        
+      //   <form className="ui form" onSubmit={this.handleFormSubmit}>
+      //   <div className="field">
+      //     <label>Username</label>
+      //     <input type="text" name="usernameInput" value={this.state.usernameInput} onChange={ e => this.handleChange(e)}/>
+      //   </div>
+      //   <div className="field"> 
+      //     <label>Password</label>
+      //     <input type="password" name="passwordInput" value={this.state.passwordInput} onChange={ e => this.handleChange(e)}/>
+      //   </div>
+       
+
+      //   <div class="ui form success">
+       
+      //   <button className="ui button" type="submit">Login</button>
+      //   </div>
+
+      // </form>
+      
+//     )
+// };
+
+export default Login;
